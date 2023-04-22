115617ee2ddd756ecf75f0ab56262bce61f793c5
vilasks <vilaskathera4@gmail.com>
2023-04-22 18:09:44 +0530

    changes for server

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const {Builder} = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
+const express = require("express")
+const app = express()
 require("chromedriver");
 
-(async function helloSelenium() {
+async function helloSelenium() {
   let driver = await new Builder().setChromeOptions(new chrome.Options().headless()).forBrowser('chrome').build();
 
   await driver.get('https://selenium.dev');
   console.log(await driver.getCurrentUrl())
   console.log("Hello")
   await driver.quit();
-})();
+}
+setTimeout(()=>{
+    helloSelenium()
+    console.log("run hello selenium")
+},10000)
+
+app.listen((err)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log("started listening on port 3000")
+    }
+},3000)
